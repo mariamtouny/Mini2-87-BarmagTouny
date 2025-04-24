039121beb9c1135f1bdea937bf71f1c005110d71
a7madgom3a16 <ahmed_gom3a88@yahoo.com>
2025-04-24 13:17:58 +0200

    Initial Rating Stuff

--- src/main/java/com/example/miniapp/repositories/RatingRepository.java
+++ src/main/java/com/example/miniapp/repositories/RatingRepository.java
@@ -1,9 +1,10 @@
 package com.example.miniapp.repositories;
 
 import com.example.miniapp.models.Rating;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
 
-@Repository
-public interface RatingRepository extends JpaRepository<Rating, String> {
+public interface RatingRepository extends MongoRepository<Rating, String> {
+    List<Rating> findByEntityIdAndEntityType(Long entityId, String entityType);
+    List<Rating> findByScoreGreaterThan(Integer score);
 }
