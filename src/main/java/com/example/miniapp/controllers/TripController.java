32016c7e799dd4294ea2bdac9b63a6fda0dfbd2f
Rafeeq Bassem <ultimaterafy@gmail.com>
2025-04-25 16:01:37 +0300

    updated deleteTrip

--- src/main/java/com/example/miniapp/controllers/TripController.java
+++ src/main/java/com/example/miniapp/controllers/TripController.java
@@ -15,35 +15,35 @@ public class TripController {
     public TripController(TripService tripService) {
         this.tripService = tripService;
     }
-    //Post request to schedule a new trip.
+
     @PostMapping("/addTrip")
     public Trip addTrip(@RequestBody Trip trip)
     {
         return tripService.addTrip(trip);
     }
 
-    //Get request to retrieve all trips
+
     @GetMapping("/allTrips")
     public List<Trip> getAllTrips()
     {
         return tripService.getAllTrips();
     }
 
-    //Get request to retrieve a trip by ID.
+
     @GetMapping("/{id}")
     public Trip getTripById(@PathVariable Long id)
     {
         return tripService.getTripById(id);
     }
 
-    //Put request to update the details of a trip.
+
     @PutMapping("/update/{id}")
     public Trip updateTrip(@PathVariable Long id, @RequestBody Trip trip)
     {
         return tripService.updateTrip(id, trip);
     }
 
-    //Delete request to cancel a trip
+
     @DeleteMapping("/delete/{id}")
     public String deleteTrip(@PathVariable Long id)
     {
@@ -51,7 +51,7 @@ public class TripController {
         return "Trip with ID " + id + " has been deleted.";
     }
 
-    //Get request to retrieve trips that occurred between two dates.
+
     @GetMapping("/findByDateRange")
     public List<Trip> findTripsWithinDateRange(@RequestParam LocalDateTime startDate, @RequestParam
     LocalDateTime endDate)
@@ -60,7 +60,7 @@ public class TripController {
 
     }
 
-    //Get request to retrieve trips associated with a specific captain.
+
     @GetMapping("/findByCaptainId")
     public List<Trip> findTripsByCaptainId(@RequestParam Long captainId)
     {
